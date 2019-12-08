cf4a7bc35ca51d8f2dd82678a6259de5464db476
Clearner5682 <625293627@qq.com>
2019-12-08 23:05:30 +0800

    修改

--- Day4/LearnVuejs04/02-webpack的使用/1.2/webpack.config.js
+++ Day4/LearnVuejs04/02-webpack的使用/1.2/webpack.config.js
@@ -5,9 +5,13 @@ module.exports={
     path:__dirname+'/dist'
   },
   module:{
+    // 对于不同文件类型的处理规则
     rules:[
       {
         test:/\.css$/,
+        // 使用的loader，顺序很重要，从右至左，类似于管道，按顺序从右至左执行
+        // css-loader负责将css代码解析成字符串
+        // style-loader负责将解析的字符串插入到html的头部
         use:['style-loader','css-loader']
       }
     ]
