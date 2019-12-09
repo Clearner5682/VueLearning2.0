0374ef37f352eb0438426f3351c05160e1b5fc76
Clearner5682 <625293627@qq.com>
2019-12-09 23:35:25 +0800

    20191209

---
+++ Day4/LearnVuejs04/02-webpack的使用/1.3loader/main.js
@@ -0,0 +1,9 @@
+let {add,multi}=require('./src/js/mathUtils');
+import {name,age,height} from "./src/js/info";
+require('./src/css/youcaihua.css');
+
+console.log(add(20, 30));
+console.log(multi(20, 30));
+console.log(name);
+console.log(age);
+console.log(height);
---
+++ Day4/LearnVuejs04/02-webpack的使用/1.3loader/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports={
+  entry:'./main.js',
+  output:{
+    filename:'bundle.js',
+    path:__dirname+'/dist'
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      }
+    ]
+  }
+}
