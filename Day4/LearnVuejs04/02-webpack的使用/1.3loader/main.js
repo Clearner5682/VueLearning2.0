6555bd536b78a20d8cca8e723d866677c7d26a34
Clearner5682 <625293627@qq.com>
2019-12-10 23:28:42 +0800

    20191210

--- Day4/LearnVuejs04/02-webpack的使用/1.3loader/main.js
+++ Day4/LearnVuejs04/02-webpack的使用/1.3loader/main.js
@@ -1,9 +1,11 @@
 let {add,multi}=require('./src/js/mathUtils');
 import {name,age,height} from "./src/js/info";
 require('./src/css/youcaihua.css');
+require('./src/css/common.less');
 
 console.log(add(20, 30));
 console.log(multi(20, 30));
 console.log(name);
 console.log(age);
 console.log(height);
+document.body.innerHTML=`<h2>你好啊，李银河</h2>`;
