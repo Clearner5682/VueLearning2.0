6ae55594d6f32574ca45a5eb36aca363880c4e30
洪岩 <dannyhong@rektec.com.cn>
2021-03-24 21:27:21 +0800

    vant的引入

---
+++ Vant的基本使用/Vant快速上手/03-通过脚手架安装/hello-world/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="primary">test</van-button>
+    <VanButton type="danger">test2</VanButton>
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+// 方式一：通过babel-plugin-import插件实现按需引入，即只有用到的组件才会被打包
+import {Button,Toast} from 'vant'
+
+// 方式二：手动按需引入
+// import Button from 'vant/lib/button'
+// import 'vant/lib/button/style'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    VanButton:Button,
+    // Toast:Toast
+  },
+  mounted(){
+    Toast.success('hello vant');
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
