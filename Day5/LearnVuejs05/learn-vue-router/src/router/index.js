283a2def0a6dca24f33117ec42045665a5fa0482
洪岩 <dannyhong@rektec.com.cn>
2021-04-27 23:15:51 +0800

    路由懒加载

--- Day5/LearnVuejs05/learn-vue-router/src/router/index.js
+++ Day5/LearnVuejs05/learn-vue-router/src/router/index.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '../components/home.vue'
-import about from '../components/about.vue'
-import user from '../components/user.vue'
+
+// 1.普通的路由加载方式（非懒加载）
+// import home from '../components/home.vue'
+// import about from '../components/about.vue'
+// import user from '../components/user.vue'
+
+// 2.懒加载-AMD写法
+// const home = resolve => require(['../components/home.vue'], resolve)
+// const about = resolve => require(['../components/about.vue'], resolve)
+// const user = resolve => require(['../components/user.vue'], resolve)
+
+// 3.懒加载-ES6的写法
+const home=()=>import('../components/home.vue')
+const about=()=>{import('../components/about.vue')}
+const user=()=>{import('../components/user.vue')}
 
 // 加上下面这行代码，防止重复调用this.$router.push()时报错
 const originalPush = Router.prototype.push
